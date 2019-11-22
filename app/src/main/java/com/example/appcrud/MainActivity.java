a067b92ea3ca4682f57eb956e8425a79aac9bdcf
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2019-11-22 15:59:08 -0300

    Telas criadas.

--- app/src/main/java/com/example/appcrud/MainActivity.java
+++ app/src/main/java/com/example/appcrud/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.appcrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ArrayList<String> mCadastros;
+
     private ListView mLvCadastros;
 
     @Override
@@ -14,17 +21,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLvCadastros =
+        mLvCadastros = findViewById(R.id.lvCadastros);
+        mLvCadastros.setOnItemClickListener(this);
     }
 
     @Override
     protected void onResume(){
         super.onResume();
 
-        //Atualizar lista aqui
+        mCadastros = new ArrayList<String>();
+
+        for (int i = 1; i <= 30; i++) {
+            mCadastros.add(String.format("Item %d", i));
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                mCadastros);
+
+        mLvCadastros.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+        Intent intent = new Intent(this, AlteraExcluiActivity.class);
+        startActivity(intent);
     }
 
     public void CadastrarOnClick(View view){
-
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
